468ffc88d4e0680f9e38d50beeceedc4a192d1a8
dhanapal <dhanapal703278@gmail.com>
2022-08-28 15:01:10 +0530

    added new terraform folder

---
+++ new_terraform/terraform_notes.sh
@@ -0,0 +1,304 @@
+##Prerequisites terraform
+To follow this tutorial you will need:
+1. The Terraform CLI (1.2.0+) installed.
+2. The AWS CLI installed.
+3. AWS account and associated credentials that allow you to create resources.
+##good content  url
+https://medium.com/appgambit/provisioning-a-jenkins-server-on-aws-with-terraform-bf04a6a6ef7f
+
+##Installation of terraform in linux of rhel/centos
+$ sudo yum install -y yum-utils
+$ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+$ sudo yum -y install terraform
+
+##Installed the AWS CLI.
+$ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+$ unzip awscliv2.zip
+$ ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+#set environment variable
+$ vim /etc/profile.d/aws.sh
+#!/bin/bash
+export PATH=$PATH:/usr/local/bin/
+:wq!
+$ bash
+$ aws --version
+
+##AWS account and associated credentials that allow you to create resources.
+#To create your access key ID and secret access key
+1.Open the IAM console at https://console.aws.amazon.com/iam/.
+2. On the navigation menu, choose Users.
+3.Choose your IAM user name (not the check box).
+4.Open the Security credentials tab, and then choose Create access key.
+5.To see the new access key, choose Show. Your credentials resemble the following:
+6.Access key ID: AKIAIOSFODNN7EXAMPLE
+7.Secret access key: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+8.To download the key pair, choose Download .csv file. Store the .csv file with keys in a secure location.
+
+##Write configuration
+The set of files used to describe infrastructure in Terraform is known as a Terraform configuration. You will write your first configuration to define a single AWS EC2 instance.
+Each Terraform configuration must be in its own working directory. 
+Create a directory for your configuration.
+$ mkdir learn-terraform-aws-instance
+$ cd learn-terraform-aws-instance
+$ touch main.tf
+
+#Open main.tf in your text editor, paste in the configuration below, and save the file.
+$ vim main.tf
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+#resource "<provider>_<resource_type>" "name" {
+Note:-Hard-coded credentials are not recommended in any Terraform configuration and risks secret leakage should this file ever be committed to a public version control system.
+
+##Adding AWS credentials
+# aws configure
+AWS Access Key ID [None]: AKIAIOSFODNN7EXAMPLE 
+AWS Secret Access Key [None]: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+Default region name [None]: ap-south-1
+Default output format [None]:
+    or
+## set credentials in the .aws configuration files
+  
+[root@ansible .aws]# vim credentials
+[default]
+aws_access_key_id =  AKIAIOSFODNN7EXAMPLE 
+aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
+:wq!
+[root@ansible .aws]# vim  config
+[default]
+region = ap-south-1
+
+:wq!
+
+##Strings and Templates
+##url for references of string and templates
+https://www.terraform.io/language/expressions/strings
+
+### variable types
+1.string 
+variable "string" {
+ default     = "ami-06a0b4e3b7eb7a300"
+}
+#2.list
+variable "list" {
+  default     = ["jenkins","docker","ansible"]
+}
+#3.map
+variable "map" {
+  default     = {
+    redhat-ami = "ami-06a0b4e3b7eb7a300"
+    amazon-ami = "ami-079b5e5b3971bd10d"
+	ubuntu-ami = "ami-068257025f72f470d"
+  }
+}
+##Terraform visualing exexution plan
+#Install graphviz.x86_64 package
+This guide let you learn how to install graphviz.x86_64 package:
+$ sudo dnf makecache
+$ sudo dnf install graphviz.x86_64
+
+#Uninstall / Remove graphviz.x86_64 package
+Please follow the step by step instructions below to uninstall graphviz.x86_64 package:
+$ sudo dnf remove graphviz.x86_64
+sudo dnf autoremove
+##To create the terraform plan graphviz
+$ terraform graph -Tsvg > graph.svg
+
+##Initialize the project.
+$ terraform init
+
+##Show changes required by the current configuration
+$ terraform plan
+
+##Check whether the configuration is valid
+Format your configuration. Terraform will print out the names of the files it modified, if any. In this case, your configuration file was already formatted correctly, so Terraform wont return any file names.
+$ terraform fmt
+
+##Check whether the configuration is valid
+$ terraform validate
+Success! The configuration is valid.
+
+##Create or update infrastructure
+Apply the configuration now with the terraform apply command. Terraform will print output similar to what is shown below. We have truncated some of the output to save space.
+$ terraform apply
+##Create or update infrastructure with auto-approve 
+#If we use "--auto-approve" it will not ask for yes or no prompt
+$ terraform apply --auto-approve 
+##Show output values from your root module
+syntax:-
+$ terraform [global options] output [options] [NAME]
+options:-
+
+  -state=path      Path to the state file to read. Defaults to
+                   "terraform.tfstate".
+
+  -no-color        If specified, output wont contain any color.
+
+  -json            If specified, machine readable output will be
+                   printed in JSON format.
+
+  -raw             For value types that can be automatically
+                   converted to a string, will print the raw
+                   string directly, rather than a human-oriented
+                   representation of the value.
+$ terraform output
+##print specific  output with
+$ terraform output <name>
+$ terraform output vpc_idl
+##Update the state to match remote systems
+$ terraform  refresh
+##update the new change to terraform code
+$ terraform apply -refresh-only
+## Show the current state or a saved plan
+ Inspect the current state using terraform show.
+$ terraform show
+
+##Key pair generating in terraform coded
+#First we need generate the public key using ssh-keygen
+$ ssh-keygen
+$ cat ~/.ssh/id_rsa.pub
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9pXjkBHlAHEI62Y/3hqXSeSYelfPFL7mZg+296hZgG5LNVnP2DKfouA8wy3tX21RsaYJcPHNlwGSSv2QJVZoxtPdJbyDDxMagWkxuBr3HbXfvq43C9f/mR7ZOSRijVGUbOTb+oRR7xyZ9RI7+ohdDoQUOPYVLd+l0L/0WdHe4j/zCA4cetabq5LFEt8+nxDQ6Nm3kLscUK7N0y5BLiJYUjVroGAq/i+oBSTpxRum6DWF1xC4+1cFrO0ihvq+Q+R3C0y/OVe97/nUxXexftgkFIgZQ9A4sX/5f93nQPKqv+/BaVzbwAFhVrDEdz4ih/OL7XVoS8UsXRHiU51/rjFF0RtwDChcjow32SS0UgmbQhyx6Fd1ai7qF8iXtQXvHIxDrxScHj1XRhM6KeIfUGci6Xx8WEzoFVtGqp7XzAtHwSjNWu4fYZXJ+V+Z/f/bpEvOgtvT0Z1D1J8QtTHTTyoXNJiySv2j2SOS2M1K5D7w2z8svUGti3jzsURmE1T/SvPk= root@ansible.cntech.local
+It will create the public and private key 
+#Now in terraform  write code  for key pair
+resoure "aws_key_pair" "terraform" {
+  key_name = "terraform-key"
+  public_key = <"here we need to add that public key">
+   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9pXjkBHlAHEI62Y/3hqXSeSYelfPFL7mZg+296hZgG5LNVnP2DKfouA8wy3tX21RsaYJcPHNlwGSSv2QJVZoxtPdJbyDDxMagWkxuBr3HbXfvq43C9f/mR7ZOSRijVGUbOTb+oRR7xyZ9RI7+ohdDoQUOPYVLd+l0L/0WdHe4j/zCA4cetabq5LFEt8+nxDQ6Nm3kLscUK7N0y5BLiJYUjVroGAq/i+oBSTpxRum6DWF1xC4+1cFrO0ihvq+Q+R3C0y/OVe97/nUxXexftgkFIgZQ9A4sX/5f93nQPKqv+/BaVzbwAFhVrDEdz4ih/OL7XVoS8UsXRHiU51/rjFF0RtwDChcjow32SS0UgmbQhyx6Fd1ai7qF8iXtQXvHIxDrxScHj1XRhM6KeIfUGci6Xx8WEzoFVtGqp7XzAtHwSjNWu4fYZXJ+V+Z/f/bpEvOgtvT0Z1D1J8QtTHTTyoXNJiySv2j2SOS2M1K5D7w2z8svUGti3jzsURmE1T/SvPk= root@ansible.cntech.local"
+}
+##Environment variables
+$ Terraform loads variables in the following order, with later sources taking precedence over earlier ones:
+1.Environment variables
+2.The terraform.tfvars file, if present.
+3.The terraform.tfvars.json file, if present.
+4.Any *.auto.tfvars or *.auto.tfvars.json files, processed in lexical order of their filenames.
+5.Any -var and -var-file options on the command line, in the order they are provided.
+ (This includes variables set by a Terraform Cloud workspace.)
+
+
+
+
+
+
+
+-----------------
+/*==== The VPC ======*/
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = "${var.environment}"
+  }
+}
+/*==== Subnets ======*/
+/* Internet gateway for the public subnet */
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name        = "${var.environment}-igw"
+    Environment = "${var.environment}"
+  }
+}
+/* Elastic IP for NAT */
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+/* NAT */
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
+  depends_on    = [aws_internet_gateway.ig]
+  tags = {
+    Name        = "nat"
+    Environment = "${var.environment}"
+  }
+}
+/* Public subnet */
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  count                   = "${length(var.public_subnets_cidr)}"
+  cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
+  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = true
+  tags = {
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-      public-subnet"
+    Environment = "${var.environment}"
+  }
+}
+/* Private subnet */
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  count                   = "${length(var.private_subnets_cidr)}"
+  cidr_block              = "${element(var.private_subnets_cidr, count.index)}"
+  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = false
+  tags = {
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
+    Environment = "${var.environment}"
+  }
+}
+/* Routing table for private subnet */
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name        = "${var.environment}-private-route-table"
+    Environment = "${var.environment}"
+  }
+}
+/* Routing table for public subnet */
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name        = "${var.environment}-public-route-table"
+    Environment = "${var.environment}"
+  }
+}
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = "${aws_route_table.private.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+}
+/* Route table associations */
+resource "aws_route_table_association" "public" {
+  count          = "${length(var.public_subnets_cidr)}"
+  subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+resource "aws_route_table_association" "private" {
+  count          = "${length(var.private_subnets_cidr)}"
+  subnet_id      = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+/*==== VPCs Default Security Group ======*/
+resource "aws_security_group" "default" {
+  name        = "${var.environment}-default-sg"
+  description = "Default security group to allow inbound/outbound from the VPC"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  }
+  
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = "true"
+  }
+  tags = {
+    Environment = "${var.environment}"
+  }
+}
