5e24e974dc1c5a4a2ead5726d83225d38a3f8d0e
T Dhanapal <t.dhanapal@inknowtech.com>
2022-10-16 20:09:03 +0530

    remove file shellscripiting

--- shellscripts/remote_ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tput setf 2
-for servername in 'cat /home/serverlist'
-do
-#ssh dhanapal@$servername sh /scripts/diskspace.sh
-ssh root@ec2-15-206-67-34.ap-south-1.compute.amazonaws.com sh /home/ec2-user/server_reachable_check.sh 
-done
